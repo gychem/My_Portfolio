e4189705951ce5ce270cd2dd70193d6751d06143
gychem <gychem.baert@hotmail.com>
2022-10-11 16:11:25 +0200

    add all projects in seperate js file

---
+++ src/projects.js
@@ -0,0 +1,110 @@
+export const projects = [
+    { 
+        "id": 1,
+        "name": "BlackJack", 
+        "slug": "blackjack",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/blackjack",
+        "image": "img/projects/blackjack.png",
+        "categories": ["game", "games", "casino", "blackjack", "fun"] , 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 2,
+        "name": "Pac-Man in Pirate Style", 
+        "slug": "pirate-pacman",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/pirate-pacman",
+        "image": "img/projects/pirate-pacman.png",
+        "categories": ["game", "games", "pirate", "pacman", "fun"] , 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 3,
+        "name": "Memory Game", 
+        "slug": "memory-game",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/memory-game",
+        "image": "img/projects/memory-game.png",
+        "categories": ["game", "games", "memory", "animals", "fun"] , 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 4,
+        "name": "Weather Forecast", 
+        "slug": "weather-api",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/weather-api",
+        "image": "img/projects/weatherapi.png",
+        "categories": ["weather", "api"] , 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 5,
+        "name": "Geocaching Tools", 
+        "slug": "geocaching",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/geocaching",
+        "image": "img/projects/geocaching.png",
+        "categories": ["geocaching", "converter"] , 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 6,
+        "name": "Geocaching Cordinate Formulas", 
+        "slug": "cord-formules",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/cord-formules",
+        "image": "img/projects/cord-formules.png",
+        "categories": ["geocaching", "converter", "cords", "coördinaten", "cordinates", "formule", "formula"] , 
+        "languages": ["javascript", "php", "mysql", "html", "css"] 
+    },       
+    { 
+        "id": 7,
+        "name": "Movie Collection", 
+        "slug": "movie-collection",
+        "openInViewer": true,
+        "url": "https://www.gychem.be/sites/movie-collection",
+        "image": "img/projects/moviecollection.png",
+        "categories": ["movies", "collection", "cards", "youtube", "json"], 
+        "languages": ["javascript", "html", "css"] 
+    },
+    { 
+        "id": 8,
+        "name": "Food Order Checkout", 
+        "slug": "food-order-checkout",
+        "openInViewer": false,
+        "url": "https://www.gychem.be/sites/food-order-checkout",
+        "image": "img/projects/order-process.png",
+        "categories": ["order", "checkout", "food", "delivery"] , 
+        "languages": ["php", "html", "css"] 
+    },
+    { 
+        "id": 9,
+        "name": "CMS (Work In Progress)", 
+        "slug": "cms",
+        "openInViewer": false,
+        "url": "https://www.cms.gychem.be/",
+        "image": "img/projects/cms.png",
+        "categories": ["order", "checkout", "food", "delivery"] , 
+        "languages": ["laravel", "php", "html", "css"] 
+    },
+    // { 
+    //     "id": 10,
+    //     "name": "Ahiru (Work In Progress)", 
+    //     "slug": "ahiru",
+    //     "openInViewer": false,
+    //     "url": "https://www.ahiru.be/",
+    //     "image": "img/projects/ahiru.png",
+    //     "categories": ["music", "platform", "collaborate", "release"] , 
+    //     "languages": ["laravel", "php", "html", "css", "vue"] 
+    // },
+    // { 
+    //     "id": 9,
+    //     "name": "🥫 Food Delivery Template", 
+    //     "slug": "food-delivery",
+    //     "image": "img/projects/fooddelivery.png",
+    //     "categories": ["food", "delivery", "template"] , 
+    //     "languages": ["css", "html"] 
+    // },
+  ];
